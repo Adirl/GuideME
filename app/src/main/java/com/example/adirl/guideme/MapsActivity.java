acc07b0d161ae5aa48bec771f5e4578598b0f0ee
Adir Lev <adirlev@gmail.com>
2017-10-30 11:17:38 +0200

    init

---
+++ app/src/main/java/com/example/adirl/guideme/MapsActivity.java
@@ -0,0 +1,65 @@
+package com.example.adirl.guideme;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Jerusalem and move the camera
+        LatLng Jerusalem = new LatLng(31.771959, 35.217018);
+        mMap.addMarker(new MarkerOptions().position(Jerusalem).title("Marker in Jerusalem"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Jerusalem));
+        mMap.setMyLocationEnabled(true);
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(31.771959, 35.217018))
+                .title("Jerusalem"));
+    }
+
+    public void onSearch (View view) {
+        EditText location_tf = (EditText)findViewById(R.id.TFaddress);
+        String location = location_tf.getText().toString();
+        List<Address> addressList = null;
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latlan = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latlan).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latlan));
+            }
+    }
+
+}
